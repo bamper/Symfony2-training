02795d43006fd9974c174109e50a66d41ce2e610
Bartłomiej Świątek <bartlomiej.swiatek@nazwa.pl>
2015-05-15 10:00:35 +0200

    zmiany w kontrolerze

--- src/Acme/HelloBundle/Controller/HelloController.php
+++ src/Acme/HelloBundle/Controller/HelloController.php
@@ -2,25 +2,21 @@
 namespace Acme\HelloBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
-//use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class HelloController
-{
-    public function indexAction($first_name, $last_name, $color)
-    {
-        return new Response('<html><body>Hello '.$first_name.' '.$last_name.' in color '.$color.'!</body></html>');
-    }
-}
-/*
+
 class HelloController extends Controller
 {
     public function indexAction($name)
     {
-        return new Response('<html><body>Hello '.$name.'!</body></html>');
-        return $this->render(
+        $content = $this->renderView(
             'AcmeHelloBundle:Hello:index.html.twig',
             array('name' => $name)
         );
+
+
+
+        return new Response($content);
+
     }
 }
-*/
