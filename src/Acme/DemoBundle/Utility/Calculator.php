f28ed7960ae80fbb3a0a40b4e6cacadc448387b7
Bartłomiej Świątek <bartlomiej.swiatek@nazwa.pl>
2015-06-25 11:12:54 +0200

    unit test

---
+++ src/Acme/DemoBundle/Utility/Calculator.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bswiatek
+ * Date: 25.06.15
+ * Time: 11:08
+ */
+namespace Acme\DemoBundle\Utility;
+
+class Calculator
+{
+    public function add($a, $b)
+    {
+        return $a + $b;
+    }
+}
